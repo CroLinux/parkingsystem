8b6889199181c15023a0496056baaa7f69d0745d
CroLinux <crolinux@gmail.com>
2023-04-13 19:57:30 +0200

    Log4j works and logs provided.

--- src/main/java/com/parkit/parkingsystem/constants/DBConstants.java
+++ src/main/java/com/parkit/parkingsystem/constants/DBConstants.java
@@ -9,5 +9,6 @@ public class DBConstants {
     public static final String UPDATE_TICKET = "update ticket set PRICE=?, OUT_TIME=? where ID=?";
     public static final String GET_TICKET = "select t.PARKING_NUMBER, t.ID, t.PRICE, t.IN_TIME, t.OUT_TIME, p.TYPE from ticket t,parking p where p.parking_number = t.parking_number and t.VEHICLE_REG_NUMBER=? order by t.IN_TIME  limit 1";
 
+    //Used to checked if the vehicle was already there in the past
     public static final String GET_FIDELITY = "select count(t.id) from ticket t where t.VEHICLE_REG_NUMBER=?";
 }
