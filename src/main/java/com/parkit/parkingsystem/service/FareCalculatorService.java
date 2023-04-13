eb1bec7ec5f6dc3cbe208aaf9c8f738dbf40a9db
CroLinux <crolinux@gmail.com>
2023-04-13 19:57:30 +0200

    Code fixed, unit tests are working!

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,5 +1,8 @@
 package com.parkit.parkingsystem.service;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
@@ -10,11 +13,17 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
-
+        //https://docs.oracle.com/javase/8/docs/api/java/time/Instant.html
+        //We convert the Date object to an Instant for the Entry and Exit values
+        Instant inHour = ticket.getInTime().toInstant();
+        Instant outHour = ticket.getOutTime().toInstant();
+        
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        //Now, using those 2 Instant values, we calculate the difference between and convert the result in Hour
+        
+        double duration = (ChronoUnit.SECONDS.between(inHour, outHour)) /3600.0;
+        // other method for the same calculation:
+        //double duration = inHour.until(outHour, ChronoUnit.SECONDS);
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
